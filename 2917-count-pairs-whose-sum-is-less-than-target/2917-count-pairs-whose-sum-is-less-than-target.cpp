308be3c2dc3a7b16b0c3a3c7a1425037b9889978
DuckVN <149903337+quocvietpham185@users.noreply.github.com>
2024-08-09 16:14:11 +0700

    Time: 4 ms (60.76%), Space: 24.1 MB (11%) - LeetHub

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int count = 0;
+        std::sort(nums.begin(), nums.end());
+        int left = 0, right = n - 1;
+        while(left < right){
+            if(nums[left] + nums[right] < target){
+                count += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+};
